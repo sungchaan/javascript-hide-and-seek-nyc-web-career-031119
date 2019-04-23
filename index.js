165502fe30cc469f8c0125f108f44b60d05a4ec5
John Lee <jslee1992@gmail.com>
2019-04-23 19:22:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.querySelector('.target')
+}
+
+function increaseRankBy(n){
+  const lists = document.querySelectorAll('.ranked-list');
+  for (let i = 0; i <lists.length; i++){
+    let children = lists[i].children;
+    for (let j = 0; j < children.length; j++){
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
+  }
+}
+
+function deepestChild(){
+  let parent = document.querySelector('div#grand-node')
+  let nextGen = parent.children[0]
+  while (nextGen){
+    parent = nextGen;
+    nextGen = parent.children[0]
+  }
+  return parent
+}
